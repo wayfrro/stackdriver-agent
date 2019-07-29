fa81f519b929dc2b81c7953b7f3a396185573965
Piotr Kowalczyk <7711184+wayfrro@users.noreply.github.com>
2019-07-29 12:48:12 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -ex
 
-docker build -f Dockerfile -t poeyashi/stackdriver-agent:latest .
-docker push poeyashi/stackdriver-agent:latest
+docker build -f Dockerfile -t wayfrro/stackdriver-agent:latest .
+docker push wayfrro/stackdriver-agent:latest
